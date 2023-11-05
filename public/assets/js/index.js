0ac6307972f1edd07216aeb29590ad6fdc24526f
Maggie McCausland <maggiemccausland@hotmail.com>
2023-11-04 21:05:16 -0600

    reverted from ids to classes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -9,9 +9,9 @@ if (window.location.pathname === '/notes') {
   noteForm = document.querySelector('.note-form');
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('#save-note');
-  newNoteBtn = document.querySelector('#new-note');
-  clearBtn = document.querySelector('#clear-btn');
+  saveNoteBtn = document.querySelector('.save-note');
+  newNoteBtn = document.querySelector('.new-note');
+  clearBtn = document.querySelector('.clear-btn');
   noteList = document.querySelectorAll('.list-container .list-group');
 }
 
