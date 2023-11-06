f2bff9f1358b211a1bccd151dc0d54b98198e9c4
Maggie McCausland <maggiemccausland@hotmail.com>
2023-11-06 15:46:23 -0700

    successfully using POST & GET

--- routes/notes.js
+++ routes/notes.js
@@ -5,4 +5,18 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+        }
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
 module.exports = notes;
