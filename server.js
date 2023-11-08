988ef94bdab3fbd1da2eda3ce97533121d9cd02b
Maggie McCausland <maggiemccausland@hotmail.com>
2023-11-08 15:25:08 -0800

    removed procfile, made minor changes to server.js to fix connection issues with heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware
@@ -16,19 +16,13 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 // Wildcard Route
-app.get('*', (req, res) =>
-res.status(404).send('<h1>404! Page not found</h1>')
-);
+// app.get('/*', (req, res) =>
+// res.status(404).send('<h1>404! Page not found</h1>')
+// );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`App is listening at: http://localhost:${PORT}`));
