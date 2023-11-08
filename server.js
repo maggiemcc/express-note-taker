34af96171bed8f3e5debd53a94a66b10ac493861
Maggie McCausland <maggiemccausland@hotmail.com>
2023-11-08 15:53:04 -0800

    chnaged * to redirect to index.html

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 // Wildcard Route for pages not found
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/404.html')));
+// app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/404.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => console.log(`App is listening at: http://localhost:${PORT}`));
